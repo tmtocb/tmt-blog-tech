5387802aecf3aa7b755cb150b1059940a05bcaf2
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-10 03:51:43 +0100

    New post fields, change CRUD actions for post_controller, assosciation between models

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < AuthorsController
 
   # GET /posts
   def index
-    @posts = Post.all
+    @posts = current_author.posts
   end
 
   # GET /posts/1
@@ -13,7 +13,7 @@ class PostsController < AuthorsController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_author.posts.build
   end
 
   # GET /posts/1/edit
@@ -22,7 +22,7 @@ class PostsController < AuthorsController
 
   # POST /posts
   def create
-    @post = Post.new(post_params)
+    @post = current_author.posts.build(post_params)
 
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
@@ -49,12 +49,12 @@ class PostsController < AuthorsController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = current_author.posts.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:title, :description, :published, :published_at, :author_id)
+      params.require(:post).permit(:title, :description)
     end
     end
 end
